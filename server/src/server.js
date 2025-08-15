87188176f1568a5a23262e2fe2407d83aff155ad
fenmow <lucas170105@outlook.com>
2025-08-14 21:25:28 -0300

    Changed server PORT conditional

--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 const API_URL = process.env.NODE_APIURL;
 app.listen(PORT, console.log(`Servidor iniciado em ${API_URL + PORT}`));
